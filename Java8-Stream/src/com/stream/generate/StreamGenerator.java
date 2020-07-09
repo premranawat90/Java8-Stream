bca77e2f21d44d7c9e838bd5ad2090128f4b7cba
Admin <Admin@DESKTOP-FDEQ9GO>
2020-07-09 22:51:52 +0530

    Initial Setup of Stream Project

---
+++ Java8-Stream/src/com/stream/generate/StreamGenerator.java
@@ -0,0 +1,23 @@
+package com.stream.generate;
+
+import java.util.stream.Stream;
+
+/**
+ * @author Prem Ranawat
+ *
+ */
+public class StreamGenerator {
+
+	public static void main(String[] args) {
+
+		System.out.println("Using Array-->");
+		int i = 0;
+		StreamGenerator.generateStreamUsingArray().forEach(y -> System.out.println(y[i]));
+	}
+
+	private static Stream<int[]> generateStreamUsingArray() {
+		int array[] = { 1, 2, 3, 4 };
+		return Stream.of(array);
+	}
+
+}
